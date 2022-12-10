b9136c894b57356af5c9f890d993a46cec570851
brenneralexandria <obrenneralexandria@gmail.com>
2022-12-10 18:29:37 -0300

    Exercicio 13

--- exercicios/exercicio13.js
+++ exercicios/exercicio13.js
@@ -1,4 +1,9 @@
-var a = parseInt(lines.shift());
-var b = parseInt(lines.shift());
-   
-console.log('X = ' + (a+b));
+var largura = prompt("Digite a largura");
+var largura = parseFloat(largura);
+
+var altura = prompt("Digite a altura");
+altura = parseFloat(altura);
+
+function MostrarArea(l, a) {
+    alert("A área total do retângulo é: " + (l * a));
+}
